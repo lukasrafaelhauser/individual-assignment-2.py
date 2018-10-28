cb929e981793a0af42a5443bb7277c4b216d89dc
Lukas Hauser <lukas.raphael.hauser@gmail.com>
2018-10-28 11:25:29 +0100

    Add bubble sort function

--- Assignment.py
+++ Assignment.py
@@ -7,3 +7,12 @@ Created on Sun Oct 28 11:17:52 2018
 """
 
 #%%
+
+def bubble(lst):
+    for j in range(0, len(lst) - 1):
+        for i in range(0, len(lst) - j - 1):
+            if lst[i] > lst[i + 1]:
+                temp = lst[i]
+                lst[i] = lst[i + 1]
+                lst[i + 1] = temp
+#%%
